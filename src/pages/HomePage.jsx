5e202e242cfa489e7d8c46e7df23b4d55d27737c
Harshit <harshit2001pandey@gmail.com>
2025-01-30 22:07:05 +0530

    Selected Chat Header

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,8 +8,8 @@ const HomePage = () => {
   return (
     <div>
       <div className="h-screen bg-base-200">
-        <div className="flex justify-center items-center pt-20 px-4">
-          <div className="text-center bg-base-100 rounded-lg shadow-xl w-full max-w-6xl h-[calc(100vh-8rem)]">
+        <div className="flex items-center justify-center pt-20 px-4">
+          <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
             <div className="flex h-full rounded-lg overflow-hidden">
               <SideBar />
               {!selectedUser ? <NoChatSelected /> : <ChatContainer />}

31eaea4b047ada7c2422bbf546d705e9876203c7
Harshit <harshit2001pandey@gmail.com>
2025-02-03 22:26:33 +0530

    project Completed

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create(
     {
-        baseURL: "http://localhost:5001/api",
+        baseURL: import.meta.env.MODE === "developlment" ? "http://localhost:5001/api": "/api",
         withCredentials: true,
     }
 )
